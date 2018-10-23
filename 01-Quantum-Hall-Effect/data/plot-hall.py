6d5a85c786a7179fb5d9a2a64a48908f1d38dcd0
Martin Pittermann <martinp2250@gmail.com>
2018-10-23 18:09:24 +0200

    01 - continued hall plot script

--- 01-Quantum-Hall-Effect/data/plot-hall.py
+++ 01-Quantum-Hall-Effect/data/plot-hall.py
@@ -5,6 +5,7 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal
 
 # for p in 4K-20uA 2K-20uA 2K-100uA; do ./plot-hall.py $p & done
 
@@ -30,15 +31,45 @@ I_res_diff = np.abs(np.diff(I_res))
 cutoff_hall = np.argmax(I_hall_diff > 1) - 1
 cutoff_res = np.argmax(I_res_diff > 1) - 1
 
-B_hall = B_hall[:cutoff_hall]
-U_hall = U_hall[:cutoff_hall]
-B_res = B_res[:cutoff_res]
-U_res = U_res[:cutoff_res]
+# cut off bad parts and simultaneously apply weiner filter, to filter weiners
+weiner_size = 11
+B_hall = scipy.signal.wiener(B_hall[:cutoff_hall], weiner_size)
+U_hall = scipy.signal.wiener(U_hall[:cutoff_hall], weiner_size) * 1000
+B_res = scipy.signal.wiener(B_res[:cutoff_res], weiner_size)
+U_res = scipy.signal.wiener(U_res[:cutoff_res], weiner_size) * 1000
 
-# plot remaining data
-plt.plot(B_hall, U_hall, label='U_h')
-plt.plot(B_res, U_res, label='U_res')
+# make figure
+fig = plt.figure()
 
-plt.legend()
+axL = fig.add_subplot(111)
+axR = axL.twinx()
+
+# align y axes
+Uh_max = np.max(U_hall)
+Uh_max = 100 * np.ceil(Uh_max / 100)
+
+axL.set_ylim(0, Uh_max)
+axR.set_ylim(0, Uh_max / 4)
+
+ticks = np.linspace(0, Uh_max, 6)
+
+axL.set_yticks(ticks)
+axR.set_yticks(ticks / 4)
+
+# plot
+axL.plot(B_hall, U_hall, '.', label='U_h')
+axR.plot(0, 0)  # skip first color
+axR.plot(B_res, U_res, '.', label='U_res')
+
+axL.set_xlabel('B (T)')
+axL.set_ylabel('$U_h$ (mV)')
+axR.set_ylabel('$U_{res}$ (mV)')
+
+fig.legend()
+
 plt.grid()
-plt.show()
+
+if len(sys.argv) == 2:
+	plt.show()
+elif len(sys.argv) >= 3:
+	plt.savefig(sys.argv[2])
