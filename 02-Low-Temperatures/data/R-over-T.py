7293d174b796afcea249f4e10eb4e55c42aabe6d
Unknown <amiculus.umut@hotmail.de>
2018-11-09 17:44:29 +0100

    02 - I want to cut myself for this

--- 02-Low-Temperatures/data/R-over-T.py
+++ 02-Low-Temperatures/data/R-over-T.py
@@ -8,33 +8,52 @@ import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf
 import numpy as np
 import tempconv as tc
+################################################################################
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('plot', choices=['overview', 'separate'],
-					help='query either overview or separate plots')
+parser.add_argument('plot', choices=['overview', 'separate', 'reduced'],
+					help='query either overview/separate/reduced plots')
 parser.add_argument('--output', type=str,
 					help='output file')
 parser.add_argument('--magnify', action='store_true',
 					help='magnify on low temperature regimes')
+parser.add_argument('--verbose', action='store_true',
+					help='print verbose output')
 args = parser.parse_args()
 
-# debye temp and resistance
-def debye(slope, off, resid):
+################################################################################
 
-	R_thet = ( resid - off ) / 0.17
+######################## function defs #########################################
+
+# debye temp and resistance
+
+
+def debye(slope, off, resid):
+	""" calculates debye temperature and resistance for a
+	given linear fit and residual resistance """
+	R_thet = (resid - off) / 0.17
 	thet = 1.17 * R_thet / slope
 	return thet, R_thet
 
+################################################################################
+
+################################################################################
+
+
 # load data
-Rtcdpt, Rcucdpt, Rsicdpt, Rnbcdpt = np.loadtxt(f'src/RoverRT-cooldown-pt.dat', unpack=True)
-Rtwapt, Rcuwapt, Rsiwapt, Rnbwapt = np.loadtxt(f'src/RoverRT-warmup-pt.dat', unpack=True)
-Rtcdc, Rcucdc, Rsicdc, Rnbcdc = np.loadtxt(f'src/RoverRT-cooldown-C.dat', unpack=True)
-Rtwac, Rcuwac, Rsiwac, Rnbwac = np.loadtxt(f'src/RoverRT-warmup-C.dat', unpack=True)
+Rtcdpt, Rcucdpt, Rsicdpt, Rnbcdpt = np.loadtxt(
+	f'src/RoverRT-cooldown-pt.dat', unpack=True)
+Rtwapt, Rcuwapt, Rsiwapt, Rnbwapt = np.loadtxt(
+	f'src/RoverRT-warmup-pt.dat', unpack=True)
+Rtcdc, Rcucdc, Rsicdc, Rnbcdc = np.loadtxt(
+	f'src/RoverRT-cooldown-C.dat', unpack=True)
+Rtwac, Rcuwac, Rsiwac, Rnbwac = np.loadtxt(
+	f'src/RoverRT-warmup-C.dat', unpack=True)
 
 # convert kOhms to Ohms
-Rtcdc = Rtcdc*1e3
-Rtwac = Rtwac*1e3
+Rtcdc = Rtcdc * 1e3
+Rtwac = Rtwac * 1e3
 
 Tcdpt = tc.int_pt100(Rtcdpt)
 
@@ -53,62 +72,118 @@ R_wa_cu = np.append(Rcuwac, Rcuwapt)
 R_wa_si = np.append(Rsiwac, Rsiwapt)
 R_wa_nb = np.append(Rnbwac, Rnbwapt)
 
-R = np.array([np.array([R_cd_cu, R_wa_cu]), np.array([R_cd_si, R_wa_si]), np.array([R_cd_nb, R_wa_nb])])
-R_c_nb = 23.58
+R = np.array([np.array([R_cd_cu, R_wa_cu]),
+			  np.array([R_cd_si, R_wa_si]),
+			  np.array([R_cd_nb, R_wa_nb])])
 
-# draw plots
+################################################################################
+
+# constants
+# to do this properly one has to detect the critical slope and take the last value before it
+resid = {
+	'Cu': np.min(R_cd_cu),
+	'Nb': 23.58
+}
+
+theta = {
+	'Cu': (0.0, 0.0),
+	'Nb': (0.0, 0.0)
+}
+
 markers = ['o', '^', 's']
 labels = ['Cu', 'Si', 'Nb']
 
+################################################################################
+
+# linear regression
+T_lin = T_cd[T_cd > 60]
+T = np.linspace(0, np.max(T_cd), 5)
+
+slps = {
+	'Cu': 0,
+	'Nb': 0
+}
+
+inters = {
+	'Cu': 0,
+	'Nb': 0
+}
+
+for r, l in zip(R, labels):
+
+	if not l == 'Si':
+		r_lin = r[0][T_cd > 60]
+		slps[l], inters[l] = np.polyfit(T_lin, r_lin, 1)
+		theta[l] = debye(slps[l], inters[l], resid[l])
+
+		if args.verbose:
+			print(
+				f'{l} -- theta = {theta[l][0]:0.2f} K, rtheta = {theta[l][1]:0.2f} Ohm')
+
+################################################################################
+
 # overview over all samples
-if args.plot == 'overview':
+if not args.plot == 'separate':
 
 	figs, ax = plt.subplots()
 	figs = [figs]
 
-	for r, l, m in zip(R, labels, markers):
-		plt.scatter(np.append(T_cd, T_wa), np.append(r[0], r[1]), label='$R_{%s}$'%l, marker=m, s=12)
+	if args.plot == 'overview':
 
-	ax.set_xlabel('$T$ (K)')
-	ax.set_ylabel('$R$ ($\\Omega$)')
-	ax.grid()
+		for r, l, m in zip(R, labels, markers):
+			plt.scatter(np.append(T_cd, T_wa), np.append(r[0], r[1]),
+						label='$R_{%s}$' % l, marker=m, s=11)
+
+		ax.set_xlabel('$T$ (K)')
+		ax.set_ylabel('$R$ ($\\Omega$)')
+
+		# zooming in on low temperature regime
+		if args.magnify:
+			plt.xlim(np.min(T_cd) - 2, 60)
+
+	else:
+
+		for r, l, m in zip(R, labels, markers):
+
+			if not l == 'Si':
+				plt.scatter(np.append(T_cd, T_wa) / theta[l][0], (np.append(r[0], r[1]) - resid[l]) / theta[l][1],
+							label=l, marker=m, s=11)
+				ax.set_xlabel('$\\frac{T}{\\theta}$')
+				ax.set_ylabel('$\\frac{R-R_{res}}{R_{\\theta}}$')
+
+				# zooming in on low temperature regime (although this isn't interesting in this case)
+				if args.magnify:
+					plt.xlim(0, 0.5)
 	ax.legend()
-
-	# zooming in on low temperature regime
-	if args.magnify:
-		plt.xlim(np.min(T_cd)-2, 60)
 
 else:
 
 	ind = np.arange(3)
-	figs = [plt.figure(n+1) for n in ind]
+	figs = [plt.figure(n + 1) for n in ind]
 	axs = [figs[n].add_subplot(111) for n in ind]
-
-	T_lin = T_cd[T_cd>60]
 
 	for fig, ax, r, l in zip(figs, axs, R, labels):
 
-		#linear regression
-		if not l == 'Si' and not args.magnify:
-			r_lin = r[0][T_cd>60]
-			T = np.linspace(0, np.max(T_cd), 5)
-			slp, inter = np.polyfit(T_lin, r_lin, 1)
-			ax.plot(T, slp*T + inter, '--', label='linear regression', color='red')
-			print('Nb -- theta = %.2f K, rtheta = %.2f Ohm'%debye(slp, inter, R_c_nb)) if l == 'Nb' else print('Cu -- theta = %.2f K, rtheta = %.2f Ohm'%debye(slp, inter, np.min(R_cd_cu)))
+		if not args.magnify and not l == 'Si':
 
-		ax.scatter(T_cd, r[0], marker='o', label='cooldown', s=12)
-		ax.scatter(T_wa, r[1], marker='s', label='warmup', s=12)
+			ax.plot(T, slps[l] * T + inters[l], '--',
+					label='linear regression', color='red')
+
+		ax.scatter(T_cd, r[0], marker='o', label='cooldown', s=11)
+		ax.scatter(T_wa, r[1], marker='s', label='warmup', s=11)
 		ax.set_xlabel('$T$ (K)')
-		ax.set_ylabel('$R_{%s}$'%l)
-		#ax.grid()
+		ax.set_ylabel('$R_{%s}$'% l)
 		ax.legend()
+
+		# zooming in on low temperature regime
 		if args.magnify:
-			ax.set_xlim(np.min(T_cd)-2, 60)
+			ax.set_xlim(np.min(T_cd) - 2, 60)
 			ax.set_ylim(0, np.max(r[1]))
 
+# if output secified, save figures
 if args.output:
 
-	pdf = matplotlib.backends.backend_pdf.PdfPages(os.path.join('plots', args.output))
+	pdf = matplotlib.backends.backend_pdf.PdfPages(args.output)
 	for fig in figs:
 		pdf.savefig(fig)
 	pdf.close()
