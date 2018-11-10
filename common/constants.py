312c7f6d64fe9e9642cb27cbf6dd5a4a72803036
Martin Pittermann <martinp2250@gmail.com>
2018-11-10 10:03:55 +0100

    all - added boltzmann constant

--- common/constants.py
+++ common/constants.py
@@ -3,7 +3,7 @@ import numpy as np
 h = 6.626070040e-34  # J s
 e = 1.6021766208e-19  # C
 c = 299792458  # m / s
-
+k = 1.38064852e-23  # J / K
 
 hbar = h / (2 * np.pi)
 magnetic_flux_quantum = h / (2 * e)
