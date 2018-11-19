8b7c0288e817dd217fe7928183c52b1fea025de1
Martin Pittermann <martinp2250@gmail.com>
2018-11-19 21:27:21 +0100

    03 - I have no idea what I'm doing

--- 03-Hall-Effect/data/eval-A.py
+++ 03-Hall-Effect/data/eval-A.py
@@ -2,10 +2,13 @@
 import argparse
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import numpy as np
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('plot', type=str, choices=['2.1', '2.2'],
+                    help='which plot to draw')
 parser.add_argument('--output', type=str,
                     help='output file')
 
@@ -15,6 +18,7 @@ args = parser.parse_args()
 temperature, current, voltage_long, voltage_hall_pos, voltage_hall_neg = np.loadtxt(
     f'src/data-A.dat', unpack=True)
 
+temperature += 273.15
 current *= 1e-3
 voltage_long *= 1
 voltage_hall_pos *= 1e-3
@@ -25,24 +29,31 @@ magnetic_field = 0.5
 resistance = voltage_long / current
 resistance_hall = (voltage_hall_pos - voltage_hall_neg) / (2 * current)
 
-hall_coefficient = - resistance_hall * 1e-3 / magnetic_field
+hall_coefficient = - resistance_hall / (1e-3 * magnetic_field)
 sheet_resistance = resistance * 10 / 19
 specific_resistance = sheet_resistance / 1e-3
 conductivity = 1 / specific_resistance
 
-if False:
+temperature_extrinsic_max = 160
+temperature_intrinsic_min = 340
+
+if args.plot == '2.1':
 	plt.plot(temperature, conductivity, 'x', label='conductivity')
 	plt.plot(temperature, 1 / hall_coefficient, 'o', label='hall coefficient')
 
 	ax = plt.gca()
 	ax.set_yscale("log", nonposy='clip')
-else:
-	plt.plot(temperature, conductivity *
-	         hall_coefficient, 'x', label='conductivity')
+
+elif args.plot == '2.2':
+	plt.plot(temperature, conductivity
+	         * hall_coefficient, 'x', label='conductivity')
 
 	ax = plt.gca()
 	ax.set_xscale("log", nonposx='clip')
 	ax.set_yscale("log", nonposy='clip')
+	xformatter = matplotlib.ticker.FormatStrFormatter('%0.0f')
+	ax.get_xaxis().set_minor_formatter(xformatter)
+	ax.get_xaxis().set_major_formatter(xformatter)
 
 plt.grid(which='both')
 plt.legend()
