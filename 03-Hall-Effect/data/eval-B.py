fc3bea8342c3c5ddcba98d3ec802a04a0770aa6d
Umut E <amiculus.umut@hotmail.de>
2018-11-21 20:47:56 +0100

    03 - no idea what I'm doing either

--- 03-Hall-Effect/data/eval-B.py
+++ 03-Hall-Effect/data/eval-B.py
@@ -17,15 +17,37 @@ args = parser.parse_args()
 
 temperature, current, voltage_long, voltage_hall_pos, voltage_hall_neg = np.loadtxt(
     f'src/data-B.dat', unpack=True)
+temperatureA, currentA, voltage_longA, voltage_hall_posA, voltage_hall_negA = np.loadtxt(
+    f'src/data-A.dat', unpack=True)
 
+# mapping B
 temperature += 273.15
 current *= 1e-6
 voltage_long *= 1e-3
 voltage_hall_pos *= 1e-3
 voltage_hall_neg *= 1e-3
 
+
+# mapping A
+temperatureA += 273.15
+currentA *= 1e-3
+voltage_longA *= 1
+voltage_hall_posA *= 1e-3
+voltage_hall_negA *= 1e-3
+
+# constants
 magnetic_field = 0.5
 
+# computations sample A
+resistanceA = voltage_longA / currentA
+resistance_hallA = (voltage_hall_posA - voltage_hall_negA) / (2 * currentA)
+
+hall_coefficientA = -1 / (resistance_hallA / (1e-3 * magnetic_field))
+sheet_resistanceA = resistanceA * 10 / 19
+specific_resistanceA = sheet_resistanceA / 1e-3
+conductivityA = 1 / specific_resistanceA
+
+# computations sample B
 delta_voltage_BD = (voltage_hall_pos - voltage_hall_neg) / 2
 
 hall_coefficient = np.abs((delta_voltage_BD / current) * (1 / magnetic_field))
@@ -46,10 +68,23 @@ if args.plot == 1:
 
 elif args.plot == 2:
 
-    pass
+    mobilityA = hall_coefficientA[temperatureA
+                                  <= 273.15] * conductivityA[temperatureA <= 273.15]
 
-plt.grid(which='both')
-plt.legend()
+    plt.plot(temperature, conductivity *
+             hall_coefficient, 'o', label='mobility sample B')
+    plt.plot(temperature, mobilityA,
+             'x', label='mobility sample A')
+
+    ax = plt.gca()
+    ax.set_yscale("log", nonposy='clip')
+    ax.set_xlabel('T (K)')
+    ax.set_ylabel('$\\log\\ \\mu$ ($\\frac{m^2}{Vs}$)')
+    xformatter = matplotlib.ticker.FormatStrFormatter('%0.0f')
+    ax.get_xaxis().set_major_formatter(xformatter)
+    plt.legend()
+
+plt.grid(which='both', linestyle='--')
 
 if args.output:
     plt.savefig(args.output)
