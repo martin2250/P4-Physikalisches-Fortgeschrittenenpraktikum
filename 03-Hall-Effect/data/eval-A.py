22a55d561745e5e177ec9bc8f08bff515c2d4d3b
Umut E <amiculus.umut@hotmail.de>
2018-11-26 15:32:09 +0100

    03 - fix eV conversion and interpolate for n_i(300K)

--- 03-Hall-Effect/data/eval-A.py
+++ 03-Hall-Effect/data/eval-A.py
@@ -10,18 +10,18 @@ import constants
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('plot', type=str, choices=['2.1', '2.2', '4', '5'],
-					help='which plot to draw')
+parser.add_argument('plot', type=str, choices=['2.1', '2.2', '4', '5+6'],
+                    help='which plot to draw')
 parser.add_argument('--output', type=str,
-					help='output file')
+                    help='output file')
 parser.add_argument('--verbose', action="store_true",
-					help='enable verbose output')
+                    help='enable verbose output')
 
 args = parser.parse_args()
 
 
 temperature, current, voltage_long, voltage_hall_pos, voltage_hall_neg = np.loadtxt(
-	f'src/data-A.dat', unpack=True)
+    f'src/data-A.dat', unpack=True)
 ################################################################################
 temperature += 273.15
 current *= 1e-3
@@ -61,17 +61,17 @@ limited_hall_coeff = hall_coefficient[limit:]
 if args.plot == '2.1':
 	plt.plot(temperature, conductivity, 'x', label='conductivity $\\sigma$')
 	plt.plot(temperature, 1 / hall_coefficient, 'o',
-			label='hall coefficient $\\frac{1}{|R_\\mathrm{H}|}$')
+          label='hall coefficient $\\frac{1}{|R_\\mathrm{H}|}$')
 	plt.ylabel('conductivity (S/m)')
 	plt.xlabel('temperature (K)')
 
 	plt.vlines(temperature_intrinsic_min, 1, 2e1)
 	plt.vlines(temperature_extrinsic_max, 1.3,
-			5, colors='C0', linestyles='dotted')
+            5, colors='C0', linestyles='dotted')
 
 	plt.text(temperature_intrinsic_min + 3, 1.3, 'purely intrinsic')
 	plt.text(temperature_intrinsic_min - 3, 1.3e1,
-			'transition region', horizontalalignment='right')
+          'transition region', horizontalalignment='right')
 
 	ax = plt.gca()
 	ax.set_yscale("log", nonposy='clip')
@@ -82,10 +82,10 @@ elif args.plot == '2.2':
 
 	plt.vlines(temperature_extrinsic_max, 0.5, 1)
 	plt.vlines(temperature_intrinsic_min, 0.14, 0.3,
-			colors='C0', linestyles='dotted')
+            colors='C0', linestyles='dotted')
 
 	plt.text(temperature_extrinsic_max - 3, 0.55,
-			'purely extrinsic', horizontalalignment='right')
+          'purely extrinsic', horizontalalignment='right')
 	plt.text(temperature_extrinsic_max + 3, 0.93, 'transition region')
 
 	ax = plt.gca()
@@ -101,7 +101,8 @@ elif args.plot == '4':
 	plt.plot(limited_temp, - n(limited_hall_coeff, limited_temp), 'x')
 
 	if args.verbose:
-		print(f'n_i(300 K) = {-n(hall_coefficient[21], temperature[21]):0.3e}')
+		n_at_300K = np.interp(300, temperature, -n(hall_coefficient, temperature))
+		print(f'n_i(300 K) = {n_at_300K:0.3e}')
 
 	ax = plt.gca()
 	#ax.set_xscale("log", nonposx='clip')
@@ -113,21 +114,22 @@ elif args.plot == '4':
 	ax.get_xaxis().set_minor_formatter(xformatter)
 	ax.get_xaxis().set_major_formatter(xformatter)
 ################################################################################
-elif args.plot == '5':
+elif args.plot == '5+6':
 	# Arrhenius representation for band gap
 	slp, inter = np.polyfit(
-			1 / limited_temp, np.log(-n(limited_hall_coeff, limited_temp) / limited_temp**(3. / 2)), 1)
+            1 / limited_temp, np.log(-n(limited_hall_coeff, limited_temp) / limited_temp**(3. / 2)), 1)
 	plt.plot(1 / limited_temp, np.log(-n(limited_hall_coeff,
-									  limited_temp) / limited_temp**(3. / 2)), '+')
-	plt.plot(1 / limited_temp, slp * 1 /
-		  limited_temp + inter, '--', color='red', )
+                                      limited_temp) / limited_temp**(3. / 2)), '+')
+	plt.plot(1 / limited_temp, slp * 1
+          / limited_temp + inter, '--', color='red', )
 	plt.xlabel('$\\frac{1}{T} \\left(\\frac{1}{K}\\right)$')
 	plt.ylabel('$\\log\\ \\frac{n_i}{T^{\\frac{3}{2}}}$')
 
 	if args.verbose:
 		Eg = -slp * 2 * constants.k
 		print(Eg)
-		print(f'Eg_0 = {Eg * 6.2415091e18:.2f} eV, offset = {inter:.2f}')
+		print(f'Eg_0 = {Eg / constants.e:.2f} eV, offset = {inter:.2f}')
+
 	plt.plot()
 ################################################################################
 plt.grid(which='both')
