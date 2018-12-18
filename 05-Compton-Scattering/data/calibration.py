573abaf76af27cdef7c1bcad30902e3541b5da04
Martin Pittermann <martinp2250@gmail.com>
2018-12-18 11:35:32 +0100

    05 - add calibration script

---
+++ 05-Compton-Scattering/data/calibration.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize
+
+
+@dataclass
+class Isotope:
+	name: str
+	energies: np.ndarray  # in eV
+	count: np.ndarray = None
+
+
+isotopes = [
+	# http://www.spectrumtechniques.com/products/sources/cobalt-57/
+	Isotope('Co-57', np.array([122.0614e3])),
+	# https://www.gammaspectacular.com/Co-60-gamma-spectrum
+	Isotope('Co-60', np.array([1173.2e3, 1332.5e3])),
+	# http://www.spectrumtechniques.com/products/sources/cesium-137/
+	# (283.53e3)
+	Isotope('Cs-137', np.array([661.657e3])),
+	# https://ehs.umich.edu/wp-content/uploads/2016/04/Sodium-22.pdf
+	Isotope('Na-22', np.array([511e3, 1274.5e3]))
+]
+
+
+def energy_to_channel_estimation(energy):
+	# ch 28: 122keV
+	# ch 362: 1132keV
+	return (energy - 122e3) * (362 - 28) / (1332e3 - 122e3) + 28
+
+################################################################################
+
+
+for isotope in isotopes:
+	data = np.load(f'src/{isotope.name}.npz')
+	isotope.count = data['count']
+	data.close()
+	del data
+
+for isotope in isotopes:
+	plt.plot(isotope.count, label=isotope.name)
+	plt.vlines([energy_to_channel_estimation(e)
+             for e in isotope.energies], 0, 12000)
+	for e in isotope.energies:
+		plt.annotate(f'{e/1e3:0.1f}',
+		             (energy_to_channel_estimation(e) + 1, 5000))
+
+################################################################################
+
+plt.legend()
+plt.xlabel('channel')
+plt.ylabel('count')
+plt.show()
