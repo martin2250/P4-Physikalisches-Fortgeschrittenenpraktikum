7e58c269c39c3cde5c51dbeb56016af6367b4eb4
martin2250 <martinp2250@gmail.com>
2018-12-27 14:58:41 +0100

    all - add avogadro number to constants

--- common/constants.py
+++ common/constants.py
@@ -4,6 +4,7 @@ h = 6.626070040e-34  # J s
 e = 1.6021766208e-19  # C
 c = 299792458  # m / s
 k = 1.38064852e-23  # J / K
+avogadro_constant = 6.022140857e23  # per mole
 
 hbar = h / (2 * np.pi)
 magnetic_flux_quantum = h / (2 * e)
