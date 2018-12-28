34264549537e97cc1030fa00830016197fef9a49
martin2250 <martinp2250@gmail.com>
2018-12-28 21:28:21 +0100

    03 - implement instructors notes

--- 03-Hall-Effect/data/eval-A.py
+++ 03-Hall-Effect/data/eval-A.py
@@ -16,10 +16,16 @@ parser.add_argument('plot', type=str, choices=['2.1', '2.2', '4', '5+6'],
                     help='which plot to draw')
 parser.add_argument('--output', type=str,
                     help='output file')
+parser.add_argument('--size', type=float, nargs=2,
+                    help='size X Y (inch)')
 parser.add_argument('--verbose', action="store_true",
                     help='enable verbose output')
 
 args = parser.parse_args()
+
+if args.size:
+	fig = matplotlib.pyplot.gcf()
+	fig.set_size_inches(*args.size)
 
 
 temperature, current, voltage_long, voltage_hall_pos, voltage_hall_neg = np.loadtxt(
@@ -66,11 +72,12 @@ limited_temp = temperature[limit:]
 limited_hall_coeff = hall_coefficient[limit:]
 ################################################################################
 if args.plot == '2.1':
-	plt.plot(temperature, conductivity, 'x', label='conductivity $\\sigma$')
+	plt.plot(temperature, conductivity, 'x',
+	         label='conductivity $\\sigma$ in S/m')
 	plt.plot(temperature, 1 / hall_coefficient, 'o',
-          label='hall coefficient $\\frac{1}{|R_\\mathrm{H}|}$')
-	plt.ylabel('conductivity (S/m)')
-	plt.xlabel('temperature (K)')
+          label='inverse hall coefficient $\\frac{1}{|R_\\mathrm{H}|}$ in m$^3$/C')
+	plt.ylabel('')
+	plt.xlabel('temperature $T$ (K)')
 
 	plt.vlines(temperature_intrinsic_min, 3, 90)
 	plt.vlines(temperature_extrinsic_max, 1, 7, colors='C0', linestyles='dotted')
@@ -93,6 +100,9 @@ elif args.plot == '2.2':
 	plt.text(temperature_extrinsic_max - 3, 0.2,
           'purely extrinsic', horizontalalignment='right')
 	plt.text(temperature_extrinsic_max + 3, 0.4, 'transition region')
+
+	plt.plot([145, 264], [0.94, 0.315], 'C1')
+	plt.plot([322, 427], [0.141, 0.079], 'C1')
 
 	ax = plt.gca()
 	ax.set_xscale("log", nonposx='clip')
@@ -122,7 +132,23 @@ elif args.plot == '4':
 ################################################################################
 elif args.plot == '5+6':
 	# Arrhenius representation for band gap
-	slp, inter, r, __, __ = scipy.stats.linregress(
+	def linear(p, x):
+		return p[0] * x + p[1]
+
+	import scipy.odr  # too lazy to scroll up
+
+	data = scipy.odr.RealData(
+            1 / limited_temp, np.log(-n(limited_hall_coeff, limited_temp) / limited_temp**(3 / 2)))  # , sx=(1 / limited_temp**2))
+
+	odr = scipy.odr.ODR(data, scipy.odr.Model(
+            linear), beta0=(-4458.922336972104, 50.90372804322858))
+
+	result = odr.run()
+
+	slp, inter = result.beta
+	slp_err, inter_err = result.sd_beta
+
+	slpb, interb, r, __, __ = scipy.stats.linregress(
             1 / limited_temp, np.log(-n(limited_hall_coeff, limited_temp) / limited_temp**(3 / 2)))
 
 	plt.plot(1 / limited_temp, np.log(-n(limited_hall_coeff,
@@ -134,16 +160,21 @@ elif args.plot == '5+6':
 
 	if args.verbose:
 		Eg = -slp * 2 * constants.k / constants.e  # in eV
-		print(f'Eg_0 = {Eg:.3f} eV, offset = {inter:.3f}')
+		Eg_err = slp_err * 2 * constants.k / constants.e  # in eV
+
+		print(
+                    f'Eg_0 = ({Eg:.3f} +- {Eg_err:.3f}) eV, offset = {inter:.3f} +- {inter_err:.3f}')
 		print(f'band gap energy (300K)= {bandgap(Eg, 300):.3f}')
 		n_i_300 = np.exp(inter + slp * (1 / 300)) * 300**(3 / 2)
-		print(f'n_i (300K) = {n_i_300:0.3e} 1/m^3')
+		n_i_300_err = (slp_err / 300 + inter_err) * n_i_300
+		print(f'n_i (300K) = ({n_i_300:0.3e} +- {n_i_300_err:0.3e}) 1/m^3')
 	plt.plot()
 	plt.legend()
 ################################################################################
 plt.grid(which='both')
+plt.tight_layout()
 
 if args.output:
-	plt.savefig(args.output)
+	plt.savefig(args.output, bbox_inches="tight", pad_inches=0.02)
 else:
 	plt.show()
